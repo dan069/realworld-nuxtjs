db1be597a1de55ca6adf4ab0bb126b40c6a56f0e
杨丹丹 <myfile@yangdandandeMBP.lan>
2021-05-06 23:38:04 +0800

    test

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">拉勾教育</h1>
+        <h1 class="logo-font">拉勾--lalalalla--教育</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
@@ -14,57 +14,51 @@
         <div class="col-md-9">
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
-              <li v-if="user" class="nav-item">
-                <nuxt-link
-                  class="nav-link"
-                  :class="{
+              <li v-if="user"
+                  class="nav-item">
+                <nuxt-link class="nav-link"
+                           :class="{
                     active: tab === 'your_feed'
                   }"
-                  exact
-                  :to="{
+                           exact
+                           :to="{
                     name: 'home',
                     query: {
                       tab: 'your_feed'
                     }
-                  }"
-                >Your Feed</nuxt-link>
+                  }">Your Feed</nuxt-link>
               </li>
               <li class="nav-item">
-                <nuxt-link
-                  class="nav-link"
-                  :class="{
+                <nuxt-link class="nav-link"
+                           :class="{
                     active: tab === 'global_feed'
                   }"
-                  exact
-                  :to="{
+                           exact
+                           :to="{
                     name: 'home'
-                  }"
-                >Global Feed</nuxt-link>
+                  }">Global Feed</nuxt-link>
               </li>
-              <li v-if="tag" class="nav-item">
-                <nuxt-link
-                  class="nav-link"
-                  :class="{
+              <li v-if="tag"
+                  class="nav-item">
+                <nuxt-link class="nav-link"
+                           :class="{
                     active: tab === 'tag'
                   }"
-                  exact
-                  :to="{
+                           exact
+                           :to="{
                     name: 'home',
                     query: {
                       tab: 'tag',
                       tag: tag
                     }
-                  }"
-                ># {{ tag }}</nuxt-link>
+                  }"># {{ tag }}</nuxt-link>
               </li>
             </ul>
           </div>
 
-          <div
-            class="article-preview"
-            v-for="article in articles"
-            :key="article.slug"
-          >
+          <div class="article-preview"
+               v-for="article in articles"
+               :key="article.slug">
             <div class="article-meta">
               <nuxt-link :to="{
                 name: 'profile',
@@ -75,7 +69,8 @@
                 <img :src="article.author.image" />
               </nuxt-link>
               <div class="info">
-                <nuxt-link class="author" :to="{
+                <nuxt-link class="author"
+                           :to="{
                   name: 'profile',
                   params: {
                     username: article.author.username
@@ -85,26 +80,22 @@
                 </nuxt-link>
                 <span class="date">{{ article.createdAt | date('MMM DD, YYYY') }}</span>
               </div>
-              <button
-                class="btn btn-outline-primary btn-sm pull-xs-right"
-                :class="{
+              <button class="btn btn-outline-primary btn-sm pull-xs-right"
+                      :class="{
                   active: article.favorited
                 }"
-                @click="onFavorite(article)"
-                :disabled="article.favoriteDisabled"
-              >
+                      @click="onFavorite(article)"
+                      :disabled="article.favoriteDisabled">
                 <i class="ion-heart"></i> {{ article.favoritesCount }}
               </button>
             </div>
-            <nuxt-link
-              class="preview-link"
-              :to="{
+            <nuxt-link class="preview-link"
+                       :to="{
                 name: 'article',
                 params: {
                   slug: article.slug
                 }
-              }"
-            >
+              }">
               <h1>{{ article.title }}</h1>
               <p>{{ article.description }}</p>
               <span>Read more...</span>
@@ -114,25 +105,21 @@
           <!-- 分页列表 -->
           <nav>
             <ul class="pagination">
-              <li
-                class="page-item"
-                :class="{
+              <li class="page-item"
+                  :class="{
                   active: item === page
                 }"
-                v-for="item in totalPage"
-                :key="item"
-              >
-                <nuxt-link
-                  class="page-link"
-                  :to="{
+                  v-for="item in totalPage"
+                  :key="item">
+                <nuxt-link class="page-link"
+                           :to="{
                     name: 'home',
                     query: {
                       page: item,
                       tag: $route.query.tag,
                       tab: tab
                     }
-                  }"
-                >{{ item }}</nuxt-link>
+                  }">{{ item }}</nuxt-link>
               </li>
             </ul>
           </nav>
@@ -145,18 +132,16 @@
             <p>Popular Tags</p>
 
             <div class="tag-list">
-              <nuxt-link
-                :to="{
+              <nuxt-link :to="{
                   name: 'home',
                   query: {
                     tab: 'tag',
                     tag: item
                   }
                 }"
-                class="tag-pill tag-default"
-                v-for="item in tags"
-                :key="item"
-              >{{ item }}</nuxt-link>
+                         class="tag-pill tag-default"
+                         v-for="item in tags"
+                         :key="item">{{ item }}</nuxt-link>
             </div>
           </div>
         </div>
@@ -179,17 +164,16 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'HomeIndex',
-  async asyncData ({ query }) {
-    const page = Number.parseInt(query.page|| 1)
+  async asyncData({ query }) {
+    const page = Number.parseInt(query.page || 1)
     const limit = 20
     const tab = query.tab || 'global_feed'
     const tag = query.tag
 
-    const loadArticles = tab === 'global_feed'
-      ? getArticles
-      : getYourFeedArticles
+    const loadArticles =
+      tab === 'global_feed' ? getArticles : getYourFeedArticles
 
-    const [ articleRes, tagRes ] = await Promise.all([
+    const [articleRes, tagRes] = await Promise.all([
       loadArticles({
         limit,
         offset: (page - 1) * limit,
@@ -201,7 +185,7 @@ export default {
     const { articles, articlesCount } = articleRes.data
     const { tags } = tagRes.data
 
-    articles.forEach(article => article.favoriteDisabled = false)
+    articles.forEach(article => (article.favoriteDisabled = false))
 
     return {
       articles, // 文章列表
@@ -216,13 +200,13 @@ export default {
   watchQuery: ['page', 'tag', 'tab'],
   computed: {
     ...mapState(['user']),
-    totalPage () {
+    totalPage() {
       return Math.ceil(this.articlesCount / this.limit)
     }
   },
 
   methods: {
-    async onFavorite (article) {
+    async onFavorite(article) {
       article.favoriteDisabled = true
       if (article.favorited) {
         // 取消点赞
@@ -242,5 +226,4 @@ export default {
 </script>
 
 <style>
-
 </style>
